e594a2f25b3fa5c8c0bab156fcde95f3fbb7cde4
Lailson Lima Nogueira <lailson.lima@gmail.com>
2021-03-27 14:55:38 -0300

    v1 finalizada

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
+const pEvent = require('p-event');
+
+const util = require('util');
 let fs = require('fs'),
     PDFParser = require("pdf2json");
 require('dotenv').config()
  
-let pdfParser = new PDFParser();
-let listaNomes = ["ZULMIRA CANDIDA DA SILVA", "ZULY CARLOS LIMA", "MARIA LAÉLIA"];
-
-//var rows = {}; // indexed by y-position
+let listaNomes = [
+"MARIA LAELIA LIMA NOGUEIRA", 
+"MARIA LAÉLIA LIMA NOGUEIRA", 
+"FRANCISCA MOREIRA DOS SANTOS", 
+"CARLOS RAMIRES GOMES", 
+"MARIA SELI MOREIRA DOS SANTOS", 
+"MARIA SELENI"];
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -22,31 +28,47 @@ const transporter = nodemailer.createTransport({
 });
 
 const vacinaUrl = "https://coronavirus.fortaleza.ce.gov.br/listaVacinacao.html";
-//const pdfFiles = "i[class='fa fa-download']";
 const linkElements = "//i[contains(@class, 'fa fa-download')]/../..";
-const nomes = ["Maria LaéLia Lima Nogueira"]
-
-var numberOfEmailsSent = 0;
+let numberOfEmailsSent = 0;
 
 (async () => {
 
-  //do{
-    await openSiteDownloadFiles(vacinaUrl, linkElements, nomes);
+  do{
+    try{
+      await openSiteDownloadFiles(vacinaUrl, linkElements, listaNomes);
+    }catch(error){
+      const mailOptions = {
+        from: process.env.user,
+        to: process.env.destination,
+        subject: 'Bot vacina caiu!!',
+        text: 'Bot vacina caiu'
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          numberOfEmailsSent++;
+        }
+      });
+      console.log(error);
+      process.exit(0);
+    }
+    console.log('Nomes não encontrados');
 
-    //console.log('Nomes não encontrados');
+    await sleep(300000); // 5 min
 
-    //await sleep(180000); // 3 min
-
-  //}while (numberOfEmailsSent < 5);
+  }while (numberOfEmailsSent < 3);
   
-  //console.log('Processo parou por excesso de emails');
-  //process.exit(1);
+  console.log('Processo parou por excesso de emails');
+  process.exit(1);
 
 })();
 
-async function openSiteDownloadFiles(url,linkElements,nomes) {
+async function openSiteDownloadFiles(url,linkElements,listaNomes) {
   const browser = await puppeteer.launch({headless:true});
   const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0); 
   await page.goto(url);
   await sleep(2000);
   const linkElementsList = await page.$x(linkElements);
@@ -56,43 +78,29 @@ async function openSiteDownloadFiles(url,linkElements,nomes) {
       let pdfUrl =  await property.jsonValue();
       let pdfName = `Lista${i}.pdf`;
       await downloadFileFromURL(pdfUrl, pdfName);
-      ///*let rows = */convertPdfToObject(pdfName).then(() => {console.log('carai' + JSON.stringify(rows) + ' s ' + pilha[0])});
-      console.log(`na teoria terminou a funcao de conversao`);
-      console.log('rows: ' + JSON.stringify(pilha));
-
-      //while (Object.keys(rows).length === 0){
-        //console.log('aguardando');
-        //sleep(1000);
-      //}
-      sleep(6000);
-      console.log('sai do while');
-      console.log(JSON.stringify(pilha));
-
-      //console.log(`rows: ${rows}`);
-      //let namesFound = await searchNamesInObject(nomes, rows);
-      let namesFound = false;
-      console.log('fim');
-      if (namesFound){
-        //Send email com lista de nomes encontrados
-        const mailOptions = {
-          from: process.env.user,
-          to: process.env.destination,
-          subject: 'Vacina disponivel!!',
-          text: 'Pessoas encontradas: ' + namesFound
-        };
-        console.log('email enviado');
-        /*transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-            numberOfEmailsSent++;
-          }
-        });*/
-      }
-      else{
-        console.log('nao achou')
-      }
+        let texto = await convertPdfToObject(pdfName, i);
+        let listaNomesEncontrados = await searchNamesInObject(texto,listaNomes);
+        if (listaNomesEncontrados.length){
+          //Send email com lista de nomes encontrados
+          const mailOptions = {
+            from: process.env.user,
+            to: process.env.destination,
+            subject: 'Vacina disponivel!!',
+            text: 'Pessoas encontradas: ' + listaNomesEncontrados
+          };
+          console.log('email enviado');
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+              numberOfEmailsSent++;
+            }
+          });
+        }
+        else{
+          console.log('nao achou');
+        }
     }    
   }
   await browser.close();
@@ -112,53 +120,34 @@ async function downloadFileFromURL(url, downloadLocation){
 	}
 }
 
-async function convertPdfToObject(pdfName){
-  //let rows = {};
-  new pdfreader.PdfReader().parseFileItems(pdfName, function (err, item) {
-    if (!item || item.page) {
-      // end of file, or page
-      printRows(rows);
-      if (item){
-        //console.log("PAGE:", item.page);
-        rows = {}; // clear rows for next page
-      }
-    } else if (item.text) {
-      // accumulate text items into rows object, per line
-      (rows[item.y] = rows[item.y] || []).push(item.text);
-    }
-  });
-}
-
-async function searchNamesInObject(nomes, pdfName){
-  console.log('vou fazer o parse');
-  
-}
-
-var pilha = [];
-let rows = {};
-
-async function printRows(rows) {
-  Object.keys(rows) // => array of y-positions (type: float)
-    .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
-    .forEach((y) => pilha.push((rows[y] || []).join("")));//console.log((rows[y] || []).join("")));
-}
-
-
-
-/*async function readPDFPages(pdfName) {
-  console.log(`lendo: ${pdfName}`);
-  const pdf = await pdfjs.getDocument(pdfName);
-  const numPages = pdf.numPages;
-  console.log(`numPages: ${numPages}`);
-  let listaPaginas = [];
-  for (let i =1; i <= numPages; i++){
-    listaPaginas[i] = i;
+async function convertPdfToObject(pdfName, fileNumber){
+  let texto;
+  let pdfParser = new PDFParser(this,1);
+  pdfParser.loadPDF(pdfName);
+  //Wait for the pdfParser_dataReady event to be finished
+  await pEvent(pdfParser, 'pdfParser_dataReady');
+  let textFileName = `createdTextFile${fileNumber}.txt`;
+  try {
+    fs.writeFileSync(textFileName, pdfParser.getRawTextContent());
+  } catch (err) {
+      console.error(err);
   }
-  const pageNumbers = Array.from(listaPaginas);
-  // Start reading all pages 1...numPages
-  const promises = pageNumbers.map(pageNo => pdf.getPage(pageNo));
-  // Wait until all pages have been read
-  const pages = await Promise.all(promises);
-  // You can do something with pages here.
-  return pages;
-}*/
+  try {
+    texto = fs.readFileSync(textFileName, 'utf8');
+    console.log(`Carregou o texto do ${textFileName} na variável texto`);
+    return texto;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function searchNamesInObject(str,substrings){
+  let listaNomesEncontrados = [];
+    for (let i = 0; i < substrings.length; i++) {
+       let substring = substrings[i].toLowerCase();
+       if (str.toLowerCase().indexOf(substring) != - 1) {
+         listaNomesEncontrados.push(substring);
+       }
+    }
+    return listaNomesEncontrados; 
+}
